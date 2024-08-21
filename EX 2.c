3993285907d8207e0a9d531ef713f7814d21b00f
jvdreersimao <joaovitordsimao@gmail.com>
2024-08-21 09:15:04 -0300

    Build a christmas tree

--- EX 2.c
+++ EX 2.c
@@ -3,23 +3,18 @@
 
     int main(){
 
-    char Aluno1 [20];
-    char Aluno2 [20] ;
-    char Aluno3 [20] ;
-    char Aluno4 [20] ;
-    int Nota1;
-    int Nota2;
-    int Nota3;
-    int Nota4;
+   printf("       X\n");
+   printf("      xxO\n");
+   printf("     Oxxxx\n");
+   printf("    xxxxxxx\n");
+   printf("   xxxxxxOxx\n");
+   printf("  xOxxxxxxxOx\n");
+   printf(" xxxxxxOxxxxxx\n");
+   printf("xxxOxxxxxxxOxxx\n");
+   printf("       xx       \n");
+   printf("       xx       \n");
+   printf("      xxxx       \n");
 
-     printf("Insira os nomes do aluno e suas respectivas notas nota: \n");
-         printf("Nomes\n");
-            scanf("%s\n%s\n%s\n%s", &Aluno1, &Aluno2,&Aluno3, &Aluno4);
-         printf("Notas\n");
-            scanf("%d\n%d\n%d\n%d", &Nota1, &Nota2,&Nota3, &Nota4);
-
-     printf("Alunos:  Notas:\n");
-      printf("%s  %d\n" "%s  %d\n" "%s  %d\n" "%s  %d\n ", Aluno1, Nota1, Aluno2,  Nota2, Aluno3, Nota3, Aluno4, Nota4 );
 
 
 
