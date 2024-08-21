af66e641a3a0fe02e9a196c928e0bb3d9f6bd7f2
jvdreersimao <joaovitordsimao@gmail.com>
2024-08-21 09:49:37 -0300

    Use this to calculate hours and minutes with seconds input

--- EX 2.c
+++ EX 2.c
@@ -3,20 +3,16 @@
 
     int main(){
 
-   printf("       X\n");
-   printf("      xxO\n");
-   printf("     Oxxxx\n");
-   printf("    xxxxxxx\n");
-   printf("   xxxxxxOxx\n");
-   printf("  xOxxxxxxxOx\n");
-   printf(" xxxxxxOxxxxxx\n");
-   printf("xxxOxxxxxxxOxxx\n");
-   printf("       xx       \n");
-   printf("       xx       \n");
-   printf("      xxxx       \n");
+        double Tempo;
+        double Horas;
+        double Minutos;
 
 
-
+        printf("Digite um tempo em segundos: \n");
+            scanf("%lf" , &Tempo  );
+        printf("Seu tempo em horas: " "%lf\n", Tempo / 3600);
+         printf("Seu tempo em minutos: " "%lf\n", Tempo / 60);
+          printf("Seu tempo em segundos: ""%lf\n", Tempo);
 
 
 
