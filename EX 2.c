3a9b82d8519bbe3d9d08a11ce274ea2413351a6e
jvdreersimao <joaovitordsimao@gmail.com>
2024-08-21 10:12:35 -0300

    Use this to calculate the avarage of three number

--- EX 2.c
+++ EX 2.c
@@ -3,18 +3,20 @@
 
     int main(){
 
-        double Tempo;
-        double Horas;
-        double Minutos;
+        double Num1;
+        double Num2;
+        double Num3;
 
-
-        printf("Digite um tempo em segundos: \n");
-            scanf("%lf" , &Tempo  );
-        printf("Seu tempo em horas: " "%lf\n", Tempo / 3600);
-         printf("Seu tempo em minutos: " "%lf\n", Tempo / 60);
-          printf("Seu tempo em segundos: ""%lf\n", Tempo);
-
-
+    printf("Insira tres valores para calcular a media \n");
+        printf("Valor Num 1: \n");
+            scanf("%lf",&Num1);
+        printf("valor Num 2: \n");
+            scanf("%lf",&Num2);
+        printf("valor Num 3: \n");
+            scanf("%lf",&Num3);
+                double Soma = Num1 + Num2 + Num3;
+                double Media = Soma / 3;
+    printf("O valor da media equivalante a: %lf\n", Media );
 
         return 0;
 }
