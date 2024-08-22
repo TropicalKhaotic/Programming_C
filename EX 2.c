d57bd18518f60c31db1da44b7b72de98abf62d89
jvdreersimao <joaovitordsimao@gmail.com>
2024-08-22 12:02:44 -0300

    Building to find the type of a triangle ### This code is to view if a three number input is a triangle, and if it is what type is it ### TODO: fix problem with the calculator not integrate with the main code.

--- EX 2.c
+++ EX 2.c
@@ -1,14 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-    int main(){
-            //Use this to consult if the number is even or odd//
-            float num;
-            printf("insert a number: \n");
-                scanf("%f", &num);
-                float num2 = fmod(num , 2);
-                (num2 == 0)? printf("%.2f is even", num) : printf ("%.2f is odd.", num);
 
+
+
+    int main(){
+
+           int num1 , num2 , num3 , ReferenceNumber;
+
+           printf("Triangle Calculator, please insert 3 numbers: \n");
+            printf("Number 1: \n");
+                scanf("%d", &num1);
+            printf("Number 2: \n");
+                scanf("%d", &num2);
+            printf("Number 3: \n");
+                scanf("%d", &num3);
+                num1 = ReferenceNumber;
+               calculo();
 
         return 0;
+
 }
+            //Fix errors with void or change it to the main code
+
+                    void calculo(){
+                             if (num2 == ReferenceNumber & num3 == ReferenceNumber){
+                                    printf("This is a Equilateral Triangle");
+                                    num2 = ReferenceNumber;
+                             } else if (num2 > ReferenceNumber){
+                                    num2 = ReferenceNumber;
+                             } else if (num3 > ReferenceNumber){
+                                    num3 = ReferenceNumber;
+                             }
