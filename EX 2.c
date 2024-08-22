8e4e893fb6090af34e9939e5047c8035e3243ab6
jvdreersimao <joaovitordsimao@gmail.com>
2024-08-22 10:50:54 -0300

    Use this if need to know if a number is even or odd ### this calculator use the math.h to know if a number is even or odd, using fmod command. in this program it was use a ternary operator to use less lines of code.

--- EX 2.c
+++ EX 2.c
@@ -2,21 +2,12 @@
 #include<stdlib.h>
 #include<math.h>
     int main(){
-
-            //its necessary to see if the ralative height input has something to do with the calculate of distance//
-        double velocidade_inicial;
-        double angulo_lacamento;
-            //  gravidade = 9.81;//
-        printf("Vamos calcular a distancia maxima de um lançamento\n");
-        printf("Insira o valor da velocidade inicial em m/s: \n");
-        scanf("%lf",&velocidade_inicial);
-        printf("Insira o valor do angulo de lancamento: \n");
-        scanf("%lf",&angulo_lacamento);
-            //Distancia = (Vi^2/G) *sen 2*(angulo//
-        double theta = angulo_lacamento;
-        double cal1 = pow(angulo_lacamento, 2) * sin(theta* 2);
-        double Distancia_Maxima = cal1/ 9.81;
-        printf ("O valor da distancia maxima e: %lf\n", cal1);
+            //Use this to consult if the number is even or odd//
+            float num;
+            printf("insert a number: \n");
+                scanf("%f", &num);
+                float num2 = fmod(num , 2);
+                (num2 == 0)? printf("%.2f is even", num) : printf ("%.2f is odd.", num);
 
 
         return 0;
