6cb528b9e8f24b4cecbd3981e6f777b97b46c88a
Rafael <rafavs09@gmail.com>
2024-08-27 08:41:41 -0300

    Fix code layout

--- EX2.c
+++ EX2.c
@@ -7,15 +7,13 @@
         int Telefone [11];
 
         printf("Bem Vindo ao programa de insercao de dados!\n");
-
         printf("Primeiro insira o seu primeiro nome: \n");
-            char Nome[20];
-            scanf("%s", &Nome);
+        char Nome[20];
+        scanf("%s", &Nome);
         printf("Agora insira o seu Telefone: \n");
-            scanf("%d", &Telefone);
+        scanf("%d", &Telefone);
         printf("Por ultimo insira o seu CEP: \n");
-            scanf("%d", &CEP);
-
+        scanf("%d", &CEP);
         printf("Nome: %s\nTelefone: %d\nCEP: %d", Nome, Telefone, CEP);
 
     return 0;
