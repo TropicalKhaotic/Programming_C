55165ea838b06e2d5815bc692caae71461deffee
Rafael <rafavs09@gmail.com>
2024-08-27 11:44:02 -0300

    Creating a code that can analise inputs and return in decreasing order

--- EX2.c
+++ EX2.c
@@ -3,18 +3,46 @@
 
     int main(){
 
-        int CEP;
-        int Telefone [11];
+        float num1, num2, num3;
+        float comparing_bigger, comparing_smaller, comparing_middle;
 
-        printf("Bem Vindo ao programa de insercao de dados!\n");
-        printf("Primeiro insira o seu primeiro nome: \n");
-        char Nome[20];
-        scanf("%s", &Nome);
-        printf("Agora insira o seu Telefone: \n");
-        scanf("%d", &Telefone);
-        printf("Por ultimo insira o seu CEP: \n");
-        scanf("%d", &CEP);
-        printf("Nome: %s\nTelefone: %d\nCEP: %d", Nome, Telefone, CEP);
+        printf("Isert 3 numbers to be compared\n");
+        scanf("%f", &num1);
+        scanf("%f", &num2);
+        scanf("%f", &num3);
+
+        if (num2 > num1 && num2 > num3){
+            comparing_bigger = num2;
+            if (num1 > num3 && num1 < comparing_bigger){
+                comparing_middle = num1;
+                comparing_smaller = num3;
+                
+                
+            } else comparing_middle = num3, comparing_smaller = num1;
+                
+                
+        }   else (num1 > num2 && num1 > num3);{
+            comparing_bigger = num1;
+            if (num2 > num3 && num2 < comparing_bigger){
+                comparing_middle = num2;
+                comparing_smaller = num3;
+                
+            } else comparing_middle = num3, comparing_smaller = num2;
+                
+            
+        }/*  else comparing_bigger = num3;
+            if (num2 > num1 && num2 < comparing_bigger && num1){
+                comparing_middle = num2;
+                comparing_smaller = num1;
+                printf("%f, %f\n", comparing_middle, comparing_smaller);
+            } else comparing_middle = num1, comparing_smaller = num2;
+                printf("%f, %f\n", comparing_middle, comparing_smaller);*/
+
+
+
+     printf("%f is the biggest number.\n%f is the number between de biggest and the smallest.\n%f is the smallest number", comparing_bigger, comparing_middle, comparing_smaller);
+
+         
 
     return 0;
-    }
+}
