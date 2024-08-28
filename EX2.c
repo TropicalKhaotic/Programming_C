e63206a874dcc4ffa5402b5ffc907ae7f7f6f0a6
Rafael <rafavs09@gmail.com>
2024-08-28 09:17:53 -0300

    Create a simple calculator

--- EX2.c
+++ EX2.c
@@ -1,16 +1,62 @@
-#include<stdio.h>
-#include<stdlib.h>
+// C Program to Make a Simple Calculator using if-else Statements 
+#include <limits.h>
+#include <stdio.h>
 
-    int main(){
+// Function that implements the simple calculator
+double simpleCalc(double num1, double num2, char op) {
+    int result;
 
-    float num1, num2;
+    // Perform the corresponding operation
+    if (op == '+') {
 
-    printf("This is a calculator, please insert two numbers: \n");
-    scanf("%f", &num1);
-    scanf("%f", &num2);
+        // Addition
+        result = num1 + num2;
+    }
+    else if (op == '-') {
 
-    printf("%f\n%f", num1, num2);
+        // Subtraction
+        result = num1 - num2;
+    }
+    else if (op == '*') {
 
+        // Multiplication
+        result = num1 * num2;
+    }
+    else if (op == '/') {
+
+        // Division
+        // Check for division by zero
+        if (num2 != 0) {
+            result = num1 / num2;
+        }
+        else {
+            printf("Error! Division by zero.\n");
+            return INT_MIN;
+        }
+    }
+    else {
+        printf("Error! Operator is not correct.\n");
+        return INT_MIN;
+    }
+
+    return result;
+}
+
+int main() {
+    char op;
+    double num1, num2;
+
+    // Read the operator
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &op);
+
+    // Read the two numbers
+    printf("Enter two operands: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    double result = simpleCalc(num1, num2, op);
+
+    printf("Result: %.2lf\n", result);
 
     return 0;
 }
