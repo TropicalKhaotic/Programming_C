2744f780d68cf8d5250f2a568bca763556562b85
Rafael <rafavs09@gmail.com>
2024-08-28 11:59:00 -0300

    Resolve print problem ### Its printing a part of IF that should not be printed, should go to the end. ### Trying to add as a void, but dont know if its gonna work properly, still need to study if its the best solution.

--- EX2.c
+++ EX2.c
@@ -13,16 +13,15 @@ igual a 100) ou reprovado (média mais nota do exame menor do que 100)*/
     float final_exam_grade;
     int final_exam;
 
-    printf ("Please insert the grade numbers: \n");
+    printf ("Please insert the grade numbers (0 - 100): \n");
     scanf ("%f\n%f", &grade1, &grade2);
     //Use to collect numbers from 0 to 100 and do the avarege operation.
-    if (BETWEEN(grade1,0,100) && BETWEEN(grade2,0,100)){
-        printf ("Grade value invalid just (0 to 100)");
+    if (grade1 > 100 | grade2 > 100){
+        printf ("Grade value invalid just (0 to 100)\n");
     } else average = (grade1 + grade2) / 2;
     //Create to analize and print out the results if the grade is enough to pass or not. In case the student dont pass it will go to revaluation.
     if (average >= 70){
-        printf ("The student is approved");
-
+        printf ("The student is approved\n");
     //This part will have a nested if, in case the professor have the grade of final exam it will be calculated if the student is approved or not.
     } if (BETWEEN(average,40,69)){
         printf("The student is under final exam.\nDo you want to insert final exam grades?\n1. Yes\n2. No\n");
@@ -30,24 +29,25 @@ igual a 100) ou reprovado (média mais nota do exame menor do que 100)*/
 
         //Final exam calculation
         if (final_exam == 1){
-            printf("Please insert final exam grades: ");
+            printf("Please insert final exam grades: \n");
             scanf("%f", &final_exam_grade);
             if (final_exam_grade <= 100 && final_exam_grade >= 0){
                 grade3 = final_exam_grade + average;
                 if (grade3 >= 100){
-                printf("The student is approved");
-                } else printf("The student is reproved");
+                printf("The student is approved\n");
+                } else printf("The student is reproved\n");
 
-            } else printf("Invalid Grade");
+            } else printf("Invalid Grade\n");
 
-        }else printf("Thanks for using our software");
+        }else printf("Thanks for using our software\n");
 
     } if (average < 40){
         printf("The student is reproved\n");
+    }
+    
+    
 
-    } else printf("Not valid input");
-
-    printf("Thanks for using our software");
+    printf("Thanks for using our software -__-");
 
     return 0;
 }
