ca6de75e5338e36304a104708b2268e20983693a
Rafael <rafavs09@gmail.com>
2024-08-28 08:13:41 -0300

    Fix problems with the calculator and make it work properly

--- EX2.c
+++ EX2.c
@@ -11,36 +11,43 @@
         scanf("%f", &num2);
         scanf("%f", &num3);
 
-        if (num2 > num1 && num2 > num3){
+       if (num1 > num2 && num1 > num3){
+            comparing_bigger = num1;
+            if (num2 > num3){
+                comparing_middle = num2;
+                comparing_smaller = num3;
+                
+                
+            } if (num3 > num2){
+                comparing_middle = num3;
+                comparing_smaller = num2;
+            }  
+        } if (num2 > num1 && num2 > num3){
             comparing_bigger = num2;
-            if (num1 > num3 && num1 < comparing_bigger){
+            if (num1 > num3){
                 comparing_middle = num1;
                 comparing_smaller = num3;
                 
                 
-            } else comparing_middle = num3, comparing_smaller = num1;
-                
-                
-        }   else (num1 > num2 && num1 > num3);{
-            comparing_bigger = num1;
-            if (num2 > num3 && num2 < comparing_bigger){
-                comparing_middle = num2;
-                comparing_smaller = num3;
-                
-            } else comparing_middle = num3, comparing_smaller = num2;
-                
+            } if (num3 > num1){
+                comparing_middle = num3;
+                comparing_smaller = num1;
+            }
             
-        }/*  else comparing_bigger = num3;
-            if (num2 > num1 && num2 < comparing_bigger && num1){
+        } if (num3 > num1 && num3 > num2){
+            comparing_bigger = num3;
+              if (num1 > num2){
+                comparing_middle = num1;
+                comparing_smaller = num2;
+                
+                
+            } if (num2 > num1){
                 comparing_middle = num2;
                 comparing_smaller = num1;
-                printf("%f, %f\n", comparing_middle, comparing_smaller);
-            } else comparing_middle = num1, comparing_smaller = num2;
-                printf("%f, %f\n", comparing_middle, comparing_smaller);*/
+            }   
+        }
 
-
-
-     printf("%f is the biggest number.\n%f is the number between de biggest and the smallest.\n%f is the smallest number", comparing_bigger, comparing_middle, comparing_smaller);
+     printf("%.3f is the biggest number.\n%.3f is the number between de biggest and the smallest.\n%.3f is the smallest number", comparing_bigger, comparing_middle, comparing_smaller);
 
          
 
