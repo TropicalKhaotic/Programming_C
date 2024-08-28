60d0a8ee5a082e367b67a8eee3705c6c10408c2c
Rafael <rafavs09@gmail.com>
2024-08-28 09:41:49 -0300

    Put description and fix error with char

--- EX2.c
+++ EX2.c
@@ -11,8 +11,20 @@
         printf("Number - 01: ");
         scanf("%f", &num1);
         printf("Now please type what kind of operator do you want (+, -, *, /): \n");
-        scanf("%c", &op);
+        scanf(" %c", &op);
+        printf("Insert what number you want to calculate\n");
+        printf("Number - 02: ");
+        scanf("%f", &num2);
 
-        
+        if (op == '+'){
+            results = num1 + num2;
+        } if (op == '-'){
+            results = num1 + num2;
+        } if (op == '*'){
+            results = num1 + num2;
+        } else results = num1 / num2;
+
+        printf("%3.f", results);
+
     return 0;
 }
