3feaf91ae72e1c95133a83a3f709379ed5e9a834
Rafael <rafavs09@gmail.com>
2024-08-28 09:20:56 -0300

    Fix issues

--- EX2.c
+++ EX2.c
@@ -1,62 +1,18 @@
-// C Program to Make a Simple Calculator using if-else Statements 
-#include <limits.h>
-#include <stdio.h>
+#include<stdlib.h>
+#include<stdio.h>
 
-// Function that implements the simple calculator
-double simpleCalc(double num1, double num2, char op) {
-    int result;
+    int main(){
 
-    // Perform the corresponding operation
-    if (op == '+') {
+        char op;
+        float num1, num2, results;
 
-        // Addition
-        result = num1 + num2;
-    }
-    else if (op == '-') {
+        printf("Welcome to TropicalKaothic Calculator\n");
+        printf("Insert what number you want to calculate\n");
+        printf("Number - 01: ");
+        scanf("%f", &num1);
+        printf("Now please type what kind of operator do you want (+, -, *, /): \n");
+        scanf("%c", &op);
 
-        // Subtraction
-        result = num1 - num2;
-    }
-    else if (op == '*') {
-
-        // Multiplication
-        result = num1 * num2;
-    }
-    else if (op == '/') {
-
-        // Division
-        // Check for division by zero
-        if (num2 != 0) {
-            result = num1 / num2;
-        }
-        else {
-            printf("Error! Division by zero.\n");
-            return INT_MIN;
-        }
-    }
-    else {
-        printf("Error! Operator is not correct.\n");
-        return INT_MIN;
-    }
-
-    return result;
-}
-
-int main() {
-    char op;
-    double num1, num2;
-
-    // Read the operator
-    printf("Enter an operator (+, -, *, /): ");
-    scanf("%c", &op);
-
-    // Read the two numbers
-    printf("Enter two operands: ");
-    scanf("%lf %lf", &num1, &num2);
-
-    double result = simpleCalc(num1, num2, op);
-
-    printf("Result: %.2lf\n", result);
-
+        
     return 0;
 }
