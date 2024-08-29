99d3b3e3f77d2b00e5c5ba97bba9764baa29342d
Rafael <rafavs09@gmail.com>
2024-08-29 11:49:59 -0300

    Remove old comments

--- EX2.c
+++ EX2.c
@@ -2,19 +2,13 @@
 #include<stdio.h>
 #define BETWEEN(value, min, max) (value <= max && value >= min)
 
+    //Use this to see if the student was approved or not
 
     int main(){
-    /*Fazer um programa que recebe duas notas parciais de um aluno (0 a 100) e informa se ele
-está aprovado (média maior ou igual a 70), em exame final (média entre 40 e 69) ou
-reprovado (média menor do que 40). Caso esteja em exame o programa deve pedir a nota do
-exame (0 a 100) e informar se o aluno está aprovado (média mais nota do exame maior ou
-igual a 100) ou reprovado (média mais nota do exame menor do que 100)*/
-
+    
     float grade1, grade2, grade3, average;
     float final_exam_grade;
     int final_exam;
-
-
 
     printf ("Please insert the grade numbers (0 - 100): \n");
     printf ("First grade: \n");
