be117a360fd250aaa2d2076514168aae5123f3ac
Rafael <rafavs09@gmail.com>
2024-09-03 11:54:50 -0300

    Use this if you need to see the days in a specific year

--- EX2.c
+++ EX2.c
@@ -5,21 +5,67 @@
 
     int main(){
     
-    int insert_max_velocity;
-    int car_velocity;
-    int multa, subtração;
+    int month;
+    int year, leap_year;
 
-    printf("Multas Vago\n");
-    printf("insert the speed limit of the avenue: ");
-    scanf("%d", &insert_max_velocity);
-    printf("Now insert the car velocity: ");
-    scanf("%d", &car_velocity);
-    if (car_velocity > insert_max_velocity){
-        subtração = car_velocity - insert_max_velocity;
-        multa = (subtração * 5);
-        printf("This fine has a value of: %d",  multa);
-            
-    }else printf("It dosent have any fine");
-    
-    return 0;
+    printf("Choose a month between 1 - 12: ");
+    scanf("%d", &month);
+    printf("Insert an year: ");
+    scanf("%d", &year);
+
+    //To check if the month input is right
+    if (month < 1 || month > 12){
+        printf("Invalid month input try numbers between 1 - 12");
+        return 0;
+    }
+    //Use this to know if its a leap year
+    if (year % 4 == 0){
+        leap_year = 1;
+    } else leap_year = 0;
+
+    //switch statmento to show the user the date he choose
+    switch (month){
+        case 1:
+          printf("Year: %d, Month: March, Days: 31.", year);
+            break;
+        case 2:
+          if (leap_year == 1){
+                 printf("Year: %d, Month: January, Days: 29.", year);
+        }   if (leap_year == 0){
+                 printf("Year: %d, Month: January, Days: 28.", year);
+        }   break;
+        case 3:
+          printf("Year: %d, Month: March, Days: 31.", year);
+            break;
+        case 4:
+          printf("Year: %d, Month: April, Days: 30.", year);
+            break;
+        case 5:
+          printf("Year: %d, Month: May, Days: 31.", year);
+            break;
+        case 6:
+         printf("Year: %d, Month: June, Days: 30.", year);
+            break;
+        case 7:
+         printf("Year: %d, Month: July, Days: 31.", year);
+            break;
+        case 8:
+         printf("Year: %d, Month: August, Days: 31.", year);
+            break;
+        case 9:
+         printf("Year: %d, Month: September, Days: 30.", year);
+            break;
+        case 10:
+         printf("Year: %d, Month: October, Days: 31.", year);
+            break;
+        case 11:
+         printf("Year: %d, Month: November, Days: 30.", year);
+            break;
+        case 12:
+         printf("Year: %d, Month: Dezember, Days: 31.", year);
+            break;
+        
+       
+    }return 0;
+
 }
