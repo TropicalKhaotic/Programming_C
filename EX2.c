f3fa26f7654df04f8d667dce2c29e8f726eca878
Rafael <rafavs09@gmail.com>
2024-09-03 09:21:26 -0300

    Starting new activity

--- EX2.c
+++ EX2.c
@@ -1,63 +1,23 @@
 #include<stdlib.h>
 #include<stdio.h>
-#define BETWEEN(value, min, max) (value <= max && value >= min)
+
 
 
     int main(){
-    /*Fazer um programa que recebe duas notas parciais de um aluno (0 a 100) e informa se ele
-está aprovado (média maior ou igual a 70), em exame final (média entre 40 e 69) ou
-reprovado (média menor do que 40). Caso esteja em exame o programa deve pedir a nota do
-exame (0 a 100) e informar se o aluno está aprovado (média mais nota do exame maior ou
-igual a 100) ou reprovado (média mais nota do exame menor do que 100)*/
-
-    float grade1, grade2, grade3, average;
-    float final_exam_grade;
-    int final_exam;
+    
+    int insert_number;
+    int dezenas;
+    int dezenas_proprias;
+    int unidades;
 
 
+        printf("Please Insert a Number between 0 - 99: ");
+        scanf("%d", &insert_number);
+        if (insert_number < 0 || insert_number > 99){
+                printf("Invalid number input, check if it is between 0 - 99 \n" );
+            
+        }else printf("%d", insert_number);
 
-    printf ("Please insert the grade numbers (0 - 100): \n");
-    printf ("First grade: \n");
-    scanf ("%f", &grade1);
-    printf("Second grade: \n");
-    scanf ("%f", &grade2);
-
-    //Use to collect numbers from 0 to 100 and do the avarege operation.
-    if (grade1 > 100 || grade2 > 100 || grade1 < 0 || grade2 < 0){
-        printf ("Grade value invalid just numbers from 0 to 100\n");
-        printf ("thanks for using our Software -__-\n");
-        return 0;
-
-    } else average = (grade1 + grade2) / 2;
-
-    //Create to analize and print out the results if the grade is enough to pass or not. In case the student dont pass it will go to revaluation.
-    if (average >= 70){
-        printf ("The student is approved\n");
-
-    //This part will have a nested if, in case the professor have the grade of final exam it will be calculated if the student is approved or not.
-    } if (BETWEEN(average,40,69)){
-        printf("The student is under final exam. Do you want to insert final exam grades?\n1. Yes\n2. No\n");
-        scanf("%d", &final_exam);
-
-        //Final exam calculation
-        if (final_exam == 1){
-            printf("Please insert final exam grades: \n");
-            scanf("%f", &final_exam_grade);
-            if (final_exam_grade <= 100 && final_exam_grade >= 0){
-                grade3 = final_exam_grade + average;
-                if (grade3 >= 100){
-                printf("The student is approved\n");
-                } else printf("The student is reproved\n");
-
-            } else printf("Invalid Grade\n");
-
-        }else printf("Thanks for using our software\n");
-        return 0;
-
-    } if (average < 40){
-        printf("The student is reproved\n");
-    }
-    printf("Thanks for using our software -__-");
 
     return 0;
 }
