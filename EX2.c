fa17e5ea040e2a2b1e2e3761d7ebb7eef9ad7528
Rafael <rafavs09@gmail.com>
2024-09-12 11:43:55 -0300

    Create a project to provide the tax based in a value input

--- EX2.c
+++ EX2.c
@@ -1,46 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define BETWEEN(value,min,max) (value <=max && value >= min)
 
   int main(){
-
-    int team_one_score, team_two_score, team_guess; 
-    int game_winner, team_one_game_score, team_two_game_score;
-    int guess_score = 0;
-
-   
-    printf("Insert your winner team guess, In case of a draw type number 3:\n ");
-    scanf("%d", &team_guess);
-    printf("Insert the score guess of each team\n");
-    printf("team one: ");
-    scanf("%d", &team_one_score);
-    printf("team two: ");
-    scanf("%d", &team_two_score);
-    printf("Now insert the game results:\n");
-
-    //Game score
-    printf("Insert the winner: ");
-    scanf("%d", &game_winner);
-    printf("Insert the score of each team\n");
-    printf("team one: ");
-    scanf("%d", &team_one_game_score);
-    printf("team two: ");
-    scanf("%d", &team_two_game_score);
-
     
-    //Evaluating if the guess is correct
-    if  (team_guess == game_winner){
-        guess_score = guess_score+10;
+    float input, calculo;
+    float month_base_value[4] = { 1637.11, 2453.50, 3271.38, 4087.65};
+    float deduzir_do_imposto[4] = { 122.78, 306.80, 552.15, 756.53 };
+    /*Por exemplo, se a base de cálculo é
+de R$10.000,00, a alíquota é de 27,5%, ou seja, R$2750,00. Deduzimos R$756,53 da parcela e
+obtemos como resultado final R$1993,47 de imposto devido.
+Escreva um programa que receba como entrada a base de cálculo mensal de um trabalhador e
+retorne o imposto de renda devido. 
+*/
+    //Base de calculo mensal
+    scanf("%lf", &input);
+    if (input < month_base_value[0]){
+        printf("Não existe nenhum imposto sobre este valor");
+    } if (BETWEEN(input,1637.11,2453.50)){
+            calculo = (input * 7.5) / 100;
+            printf("%f", calculo);
 
-    }if (team_one_score == team_one_game_score && team_two_score == team_two_game_score){
-         guess_score = guess_score+10;
-        return 0;
-
-    }if (team_one_score == team_one_game_score || team_two_score == team_two_game_score){
-        guess_score = guess_score+5;
-
-    } else;
-
-    printf("The score your team gets from the guess game is: %d", guess_score);
+    }else
    return 0;
 
 }
