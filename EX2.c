ba5eb75d25816182899dbd16e989c59b409fe382
Rafael <rafavs09@gmail.com>
2024-09-13 09:18:53 -0300

    Create a area calculator Need to know if the math expressions is correct

--- EX2.c
+++ EX2.c
@@ -1,39 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define BETWEEN(value,min,max) (value <=max && value >= min)
+
 
   int main(){
-    
-    float input, calculo;
-    float month_base_value[4] = { 1637.11, 2453.50, 3271.38, 4087.65};
-    float deduzir_do_imposto[4] = { 122.78, 306.80, 552.15, 756.53 };
-    /*Por exemplo, se a base de cálculo é
-de R$10.000,00, a alíquota é de 27,5%, ou seja, R$2750,00. Deduzimos R$756,53 da parcela e
-obtemos como resultado final R$1993,47 de imposto devido.
-Escreva um programa que receba como entrada a base de cálculo mensal de um trabalhador e
-retorne o imposto de renda devido. 
-*/
-    //Base de calculo mensal
-    printf("Insira o valor que deseja para o calculo de imposto de renda: \n");
-    scanf("%f", &input);
-    if (input <= month_base_value[0]){
-        printf("Não existe nenhum imposto sobre este valor");
-        return 0;
-    } if (BETWEEN(input,month_base_value[0],month_base_value[1])){
-            calculo = (input * 7.5) / 100;
-            calculo = calculo - deduzir_do_imposto[0];
-    } if (BETWEEN(input,month_base_value[1],month_base_value[2])){
-            calculo = (input * 15) / 100;
-            calculo = calculo - deduzir_do_imposto[1];
-    } if (BETWEEN(input,month_base_value[2],month_base_value[3])){
-            calculo = (input * 22.5) / 100;
-            calculo = calculo - deduzir_do_imposto[2];
-    } if (input > month_base_value[3]){
-            calculo = (input * 27.5) / 100;
-            calculo = calculo - deduzir_do_imposto[3]; 
-    }else
 
-     printf("O importo de renda que o usuario deve pagar é: %.2f", calculo);
+    int calculator_type;
+    float side1, side2, calculator;
+
+    printf("Please insert the number of sides: \n");
+    scanf("%d", &calculator_type);
+    switch (calculator_type){
+    case 3:
+      printf("Please insert the value of the side: \n");
+      scanf("%f", &side1 /*side2*/);
+      calculator = (side1 * side1 * 1.73 / 4);
+      printf("The area of the triangle is: %f", calculator);
+      break;
+
+    case 4:
+      printf("Please insert the value of the side: \n");
+      scanf("%f", &side1 /*side2*/);
+      calculator = side1 * side1;
+      printf("The area of the square is: %f", calculator);
+      break;
+    case 6:
+      printf("Please insert the value of the side: \n");
+      scanf("%f%f", &side1 /*side2*/);
+      calculator = 6 * (side1 * side1 * 1.73 / 4);
+      printf("The area of the hexagon is: %f", calculator);
+      break;
+
+    default: 
+      printf("Não sei calcular a area");
+      break;
+    }
+
    return 0;
 
 }
