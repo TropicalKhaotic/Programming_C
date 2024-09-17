4ef749817c6b9d2298844b3b21b46c7a24f5926a
Rafael <rafavs09@gmail.com>
2024-09-17 11:09:25 -0300

    Create a way to put concat numbers into an array Need to create a comparison way, to finish the activity

--- EX2.c
+++ EX2.c
@@ -1,40 +1,65 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <string.h> 
 
 
-  int main(){
+   int concat(int num1, int num2){
+        char s1[20];
+        char s2[20];
 
-    int calculator_type;
-    float side1, side2, calculator;
+        sprintf(s1, "%d", num1);
+        sprintf(s2, "%d", num2);
+        strcat(s1, s2);
+        int c = atoi(s1);
+        return c;
+   }
 
-    printf("Please insert the number of sides: \n");
-    scanf("%d", &calculator_type);
-    switch (calculator_type){
-    case 3:
-      printf("Please insert the value of the side: \n");
-      scanf("%f", &side1 /*side2*/);
-      calculator = (side1 * side1 * 1.73 / 4);
-      printf("The area of the triangle is: %f", calculator);
-      break;
+   int main(){
 
-    case 4:
-      printf("Please insert the value of the side: \n");
-      scanf("%f", &side1 /*side2*/);
-      calculator = side1 * side1;
-      printf("The area of the square is: %f", calculator);
-      break;
-    case 6:
-      printf("Please insert the value of the side: \n");
-      scanf("%f%f", &side1 /*side2*/);
-      calculator = 6 * (side1 * side1 * 1.73 / 4);
-      printf("The area of the hexagon is: %f", calculator);
-      break;
 
-    default: 
-      printf("Não sei calcular a area");
-      break;
+    int num1 = 0 , num2 = 0;
+    int multiplicação, soma, contador;
+    int size = 0;
+    int n = 0;
+    int initial_size = 10;
+    int *array = (int *)malloc(initial_size * sizeof(int));
+
+    soma = num1 + num2;
+    multiplicação = soma * soma;
+
+    if (array == NULL) {
+     printf("Memory reallocation failed\n");
+     return 1;
     }
-
+    
+    while(concat(num1, num2) <= 9999){
+      if(size >= initial_size){
+        initial_size *= 2;
+        array = (int *)realloc(array, initial_size * sizeof(int));
+      } if (array == NULL) {
+         printf("Memory reallocation failed\n");
+         return 1;
+      }
+      
+      array[size] = concat(num1,num2);
+      size++;
+      num1++;
+      num2++;
+    } 
+    /*printf("Numbers in the array:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+        }*/
+    while (contador != size){
+      if (multiplicação && array[n]){
+        printf("%d\n%d", multiplicação, array);
+        n++;
+        contador++;
+        
+      }
+      
+    }
+    
    return 0;
-
+    
 }
