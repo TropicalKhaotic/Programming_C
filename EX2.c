3e7852e55864f42128a172b647c84102a417a49b
Rafael <rafavs09@gmail.com>
2024-09-19 12:01:03 -0300

    Programing a analizer

--- EX2.c
+++ EX2.c
@@ -3,19 +3,33 @@
 
 int main(){
 
-        int num1 = 0;
-        int Fibonacci = 0;
-        int count = 0;
+        int num1 = 1; 
+        int soma, calculo, divisor;
+        
+        while (num1 <= 100){
+            soma = 0;
+            divisor = 1;
 
-        while (count <= 15){
+                while (divisor < num1 ){
+
+                    calculo = num1 % divisor;
+                    
+                    if (calculo == 0){
+                        soma += divisor;
+                    } divisor++;
+                }
+
+                if (soma == num1){
+                    printf ("%d", soma);
+                    }
+        } num1++;
+        
+                
             
-            Fibonacci = (num1 - 1) + (num1 - 2);
-            if (Fibonacci <= 0){
-                printf("0\n");
-            } else printf("%d\n", Fibonacci);
-            count++;
-            num1++;
-        }
+        
+            
+        
+        
         
 return 0;
 
