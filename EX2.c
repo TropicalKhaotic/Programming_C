7f7cb3eb90bda75b8cd8cb2c1828ad2983923866
Rafael <rafavs09@gmail.com>
2024-09-20 10:09:18 -0300

    Changed name

---
+++ EX2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+    int main(){
+
+    printf("XXXXXXXX\n");
+    printf("X      X\n");
+    printf("X      X\n");
+    printf("X      X\n");
+     printf("XXXXXXXX\n");
+
+
+
+    }
