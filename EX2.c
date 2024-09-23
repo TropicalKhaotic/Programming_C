993476811a0dec33165b8988a5986b594d120ae1
Rafael <rafavs09@gmail.com>
2024-09-23 11:16:10 -0300

    Fix Calculator

--- EX2.c
+++ EX2.c
@@ -4,12 +4,14 @@
 #define PI  3.14159 / 180
 int main(){
 
+    //TODO Create a way to input the altura_lançamento into the calculator
+
     double altura_lançamento, velocidade_inicial, angulo_lançamento, tempo_sec;
     double Altura_max, Alcance, voy, vox, ang, X, Y;
     
     printf("Please insert the numbers to calculate: \n");
-    printf("altura_lançamento \n");
-    scanf("%lf", &altura_lançamento);
+    /*printf("altura_lançamento \n");
+    scanf("%lf", &altura_lançamento);*/
     printf("velocidade inicial \n");
     scanf("%lf", &velocidade_inicial);
     printf("angulo de lançamento \n");
@@ -25,7 +27,7 @@ int main(){
     float dados_2 = Altura_max / 5;
 
     for (int tamanho = 0; tamanho <= 10 ; tamanho++){
-        
+
         if(tamanho <= 5){
             X = dados * tamanho;
             Y = dados_2 * tamanho;
@@ -34,16 +36,7 @@ int main(){
             Y = Y - dados_2;
         }
 
-        printf("X %.2lf, Y %.2lf", X, Y);
-        printf("%d\n", tamanho);
+        printf("X %.2f, Y %.2f\n", X, Y);
     }
-    
-    
-
-return 0;
-
-
-
-
-
+    return 0;
 }
