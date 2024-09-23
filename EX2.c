9c6e399fbd7e8b09e88b63f0dada9b52a0388ca5
Rafael <rafavs09@gmail.com>
2024-09-23 11:03:01 -0300

    Lançamento oblico Calculator

--- EX2.c
+++ EX2.c
@@ -1,33 +1,45 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<math.h>
+#define PI  3.14159 / 180
 int main(){
 
+    double altura_lançamento, velocidade_inicial, angulo_lançamento, tempo_sec;
+    double Altura_max, Alcance, voy, vox, ang, X, Y;
+    
+    printf("Please insert the numbers to calculate: \n");
+    printf("altura_lançamento \n");
+    scanf("%lf", &altura_lançamento);
+    printf("velocidade inicial \n");
+    scanf("%lf", &velocidade_inicial);
+    printf("angulo de lançamento \n");
+    scanf("%lf", &angulo_lançamento);
 
-        int number_input, resultado;
-        int divisor = 1, contador = 0;
-        
-        printf("PLease insert a number: \n");
-        scanf("%d", &number_input);
-        
-        while (divisor <= number_input){
-            resultado = number_input % divisor;
-                if (resultado == 0){
-                    contador++;
-                    
+    ang = angulo_lançamento * PI;
+    voy = velocidade_inicial * sin(ang);
+    vox = velocidade_inicial * cos(ang);
+    tempo_sec = voy / 10;
+    Altura_max = pow(voy, 2) / 20;
+    Alcance = vox * (tempo_sec * 2);
+    float dados = Alcance / 10;
+    float dados_2 = Altura_max / 5;
 
-                } divisor++;
-               
-                
-            } if (contador == 2){
-                printf("O numero %d é primo", number_input);
-                return 0;
+    for (int tamanho = 0; tamanho <= 10 ; tamanho++){
+        
+        if(tamanho <= 5){
+            X = dados * tamanho;
+            Y = dados_2 * tamanho;
+        } if (tamanho > 5 ){
+            X = dados * tamanho;
+            Y = Y - dados_2;
+        }
 
-            } printf("O numero %d não é primo", number_input);
-            
-            
-        
-        
+        printf("X %.2lf, Y %.2lf", X, Y);
+        printf("%d\n", tamanho);
+    }
+    
+    
+
 return 0;
 
 
