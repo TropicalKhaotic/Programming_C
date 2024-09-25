3c3378d119a70364d73f98ced4e2e7a57414ce17
Rafael <rafavs09@gmail.com>
2024-09-25 11:42:56 -0300

    Create a MMC Calculator

--- EX2.c
+++ EX2.c
@@ -3,19 +3,55 @@
 
 int main(){
 
-    int num1, num2;
+    int num1, num2, rest1, rest2;
+    int multiplicação = 1;
+    int quocient[] = {2,3,5,7,11,13,17,19};
+    int num = 0;
 
-    printf("Insert the number to be devided: ");
+    printf("Insert the number: ");
     scanf("%d", &num1);
-    printf("Insert the dividend number: ");
+    printf("Insert the number: ");
     scanf("%d", &num2);
 
-    while (num1 >= 1){
-                  
+    while (num1 != 1 && num2 != 1){
+        rest1 = num1 % quocient[num];
+        rest2 = num2 % quocient[num];
+        printf("%d\n", num1);
+        printf("%d\n", num2);
+        printf("\n\n\n");
+
+        if (rest1 == 0 && rest2 == 0){
+            num1 = num1 / quocient[num];
+            num2 = num2 / quocient[num];
+            multiplicação = multiplicação * quocient[num];
+            num = 0;
+
+        } if (rest1 != 0 || rest2 != 0){
+            if (rest1 == 0){
+            num1 = num1 / quocient[num];
+            num = 0;
+            } if (rest2 == 0){
+            num2 = num2 / quocient[num];
+            num = 0;
+            }
+        } if (num != 1 || num1 != 1){
+            if (rest1 == 0){
+            num1 = num1 / quocient[num];
+            num = 0;
+            } if (rest2 == 0){
+            num2 = num2 / quocient[num];
+            
+            }
+        }
+    num++;
+
     }
 
-    printf("Seu MDC é equivalante a: %d", num1);
-    
+    printf("%d\n\n\n", num1);
+    printf("%d\n\n\n", num2);
+
+
+
     
     
 return 0;
