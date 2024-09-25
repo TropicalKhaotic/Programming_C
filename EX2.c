84d914a2a0a8a6ba71e25db8d2f0a715d671521d
Rafael <rafavs09@gmail.com>
2024-09-25 09:15:08 -0300

    Create a dividend calculator with prime numbers

--- EX2.c
+++ EX2.c
@@ -3,25 +3,30 @@
 
 int main(){
 
+    int num1;
+    float rest;
+    int num = 0;
+    int quocient[] = {2,3,5,7,11,13,17,19};
 
-    int num1,num2;
-    int contador;
-    int resultado;
-
-    printf("Please insert a number to be calculated\nThe dividend number: \n");
+    printf("Please insert a number: ");
     scanf("%d", &num1);
-    printf("Insert Quotient number: \n");
-    scanf("%d", &num2);
-    resultado = num1;
+    printf("%d\n", num1);
 
-    while (resultado > 0 && resultado >= num2){
-        resultado = resultado - num2;
-        contador++;   
+    while (num1 >= 0){
+    rest = num1 % quocient[num];
+    if (num1 == 1){
+        return 0;
+    } if (rest == 0){
+        num1 = num1 / quocient[num];
+        num = 0;
+        printf("%d\n",num1);
+
+    } if (rest > 0) {
+        num++;
+    }
     }
     
-    printf("O quociente é: %d\n", contador);
-    printf("O resto da divisão é: %d\n", resultado);
-        
+    
 return 0;
-
 }
+
