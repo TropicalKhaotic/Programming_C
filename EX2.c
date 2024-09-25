5afb9286ea41f1bf3c1bdace4e6717d2001b8246
Rafael <rafavs09@gmail.com>
2024-09-25 09:47:32 -0300

    Create a MDC calculator

--- EX2.c
+++ EX2.c
@@ -3,28 +3,21 @@
 
 int main(){
 
-    int num1;
-    float rest;
-    int num = 0;
-    int quocient[] = {2,3,5,7,11,13,17,19};
+    int num1, num2;
 
-    printf("Please insert a number: ");
+    printf("Insert the number to be devided: ");
     scanf("%d", &num1);
-    printf("%d\n", num1);
+    printf("Insert the dividend number: ");
+    scanf("%d", &num2);
 
-    while (num1 >= 0){
-    rest = num1 % quocient[num];
-    if (num1 == 1){
-        return 0;
-    } if (rest == 0){
-        num1 = num1 / quocient[num];
-        num = 0;
-        printf("%d\n",num1);
+    while (num2 != 0){
+        int b = num2;
+        num2 = num1 % num2;
+        num1 = b;          
+    }
 
-    } if (rest > 0) {
-        num++;
-    }
-    }
+    printf("Seu MDC é equivalante a: %d", num1);
+    
     
     
 return 0;
