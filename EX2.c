21f68845c08d4d608707fd1559e2283e7a268f34
Rafael <rafavs09@gmail.com>
2024-09-26 09:15:05 -0300

    Create a MMC calculator

--- EX2.c
+++ EX2.c
@@ -13,42 +13,61 @@ int main(){
     printf("Insert the number: ");
     scanf("%d", &num2);
 
-    while (num1 != 1 && num2 != 1){
+    while (num1 != 1 || num2 != 1){
+
         rest1 = num1 % quocient[num];
         rest2 = num2 % quocient[num];
-        printf("%d\n", num1);
-        printf("%d\n", num2);
-        printf("\n\n\n");
 
-        if (rest1 == 0 && rest2 == 0){
+        if(rest1 == rest2 && rest1 == 0){
+
             num1 = num1 / quocient[num];
             num2 = num2 / quocient[num];
             multiplicação = multiplicação * quocient[num];
             num = 0;
 
         } if (rest1 != 0 || rest2 != 0){
-            if (rest1 == 0){
-            num1 = num1 / quocient[num];
-            num = 0;
-            } if (rest2 == 0){
-            num2 = num2 / quocient[num];
-            num = 0;
-            }
-        } if (num != 1 || num1 != 1){
-            if (rest1 == 0){
-            num1 = num1 / quocient[num];
-            num = 0;
-            } if (rest2 == 0){
-            num2 = num2 / quocient[num];
-            
-            }
-        }
-    num++;
 
+            if (rest1 == 0){
+            num1 = num1 / quocient[num];
+            multiplicação = multiplicação * quocient[num];
+            num = 0;
+
+            } if (rest2 == 0){
+            num2 = num2 / quocient[num];
+            multiplicação = multiplicação * quocient[num];
+            num = 0;
+            }
+
+
+        } if (num1 == 1 || num2 == 1){
+
+            while (num1 != 1 || num2 != 1){
+                rest1 = num1 % quocient[num];
+                printf("%d\n", rest1);
+                rest2 = num2 % quocient[num];
+
+                if (num1 == 1){
+                    if (rest2 == 0){
+                    num2 = num2 / quocient[num];
+                    multiplicação = multiplicação * quocient[num];
+                    num = 0;
+                    }
+
+                } if (num2 == 1){
+                    if (rest1 == 0){
+                    num1 = num1 / quocient[num];
+                    multiplicação = multiplicação * quocient[num];
+                    num = 0;
+                    }
+
+                } 
+                num++;
+            }
+
+        } num++; 
+        
     }
-
-    printf("%d\n\n\n", num1);
-    printf("%d\n\n\n", num2);
+    printf("%d", multiplicação);
 
 
 
