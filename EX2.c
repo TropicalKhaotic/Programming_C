a96ea9d0d4d3fbd734e835a2f36af632b90bedaf
Rafael <rafavs09@gmail.com>
2024-09-30 11:24:37 -0300

    Create a software tat calculate how many 4 numbers are in a domino game

--- EX2.c
+++ EX2.c
@@ -1,33 +1,51 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<time.h>
-#include<windows.h>
+#include<math.h>
 
 int main(){
 
-    int click_input;
-   
-    printf("Teste de Velocidade de Resposta do Usuario: Digite o numero que aparecera apos um tempo aleatorio\n");
+   int domino_numbers = 28;
+   int contador = 1;
+   int four_number = 1;
+   int num = 0;
+   int calibration = 0;
+  
 
-    for (int quantidade = 1; quantidade < 5; quantidade++){
-    int random_sleeptime = rand () % 10000 + 1000;
-    int random_number = rand() % 1000 + 3;
-             
-    clock_t ti,tf;
-    srand(time(NULL));
-    Sleep(random_sleeptime);
-    printf("%d\n", random_number);
-    ti = clock();
-    scanf("%d", &click_input);
-    tf = clock() - ti;
+    while (num < domino_numbers){
+        num += contador;
+        
+            
+        if (contador == 5) {
+            four_number = four_number + contador;
+            
+            four_number++;
+            
+            }
 
-    if (click_input == random_number){
-        printf("O tempo de Resposta foi: %.4lf Segundos.\n\n", (double)tf / CLOCKS_PER_SEC);
-    } if (click_input != random_number){
-        printf("Você errou o numero :/\n\n");
+        if (contador > 5) {
+          while (calibration + num < domino_numbers){
+                calibration = calibration;
+                calibration++;
+
+                
+            } 
+
+            if ( calibration > contador ) {
+                while ( calibration > 5){
+                    four_number++;
+                    calibration = calibration - contador;
+                    contador++;
+                }
+            }
+        }
+           
+
+    contador++;
+
     }
-    
-    }
 
-    return 0;
+    printf("%d\n", four_number);
+ 
+
+   return 0;
 }
