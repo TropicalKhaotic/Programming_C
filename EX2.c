44f7b873728611a986be3e19df19c745f9e8db98
Rafael <rafavs09@gmail.com>
2024-10-01 10:34:01 -0300

    Create a number 3 analizer

--- EX2.c
+++ EX2.c
@@ -3,23 +3,74 @@
 
 int main(){
 
-    int num_input = 0;
-    int sum = 0;
-    int results = 0;
-    int count = 0;
+    int num_input = 3;
+ 
+    int num_1 = 0;
+    int num_2 = 0;
+    int verification = 0;
 
-    for ( num_input; num_input >= 0; count++) {
-        scanf("%d", &num_input);
+        if ( num_input >= 1000 && num_input <= 9999) {
+            num_1 = num_input / 100;
+            num_2 = num_input % 100;
+            num_1 = num_1 / 10;
+            num_2 = num_2 / 10;
+            if (num_1 == 3 || num_2 == 3) {
+                printf("O numero %d possui o numero 3 em sua composição", num_input);
+                
+            } 
+            
+            if (num_1 != 3 && num_2 != 3) {
+                num_1 = num_input / 100;
+                num_2 = num_input % 100;
+                num_1 = num_1 % 10;
+                num_2 = num_2 % 10;
+                if (num_1 == 3 || num_2 == 3) {
+                    printf("O numero %d possui o numero 3 em sua composição", num_input);
+                    
 
-        if (num_input > 0){
-        sum = sum + num_input;
+                } else printf("O numero %d não possui o numero 3 em sua composição", num_input);
+                    
+                
+
+            } 
+            
         }
+        if ( num_input >= 100 && num_input <= 999) {
+            num_1 = num_input / 100;
+            num_2 = num_input % 100;
+            num_2 = num_2 / 10;
+            if ( num_1 == 3 || num_2 ==3 ) {
+                printf("O numero %d possui o numero 3 em sua composição", num_input);
+                
+            } 
+            
+            if ( num_1 != 3 && num_2 != 3 ) {
+                num_2 = num_input % 100;
+                num_2 = num_2 % 10;
+                if ( num_2 == 3) {
+                    printf("O numero %d possui o numero 3 em sua composição", num_input);
+                    return 0;
 
-    } count--;
-    
-    results = sum / count;
-    printf("%d", results);
+                } else printf("O numero %d não possui o numero 3 em sua composição", num_input);
+            }
+        }
+        if ( num_input >= 10 && num_input <= 99) {
+            num_1 = num_input / 10;
+            num_2 = num_input % 10;
+            if ( num_1 == 3 || num_2 ==3 ) {
+                printf("O numero %d possui o numero 3 em sua composição", num_input);
+                return 0;
 
+            } printf("O numero %d não possui o numero 3 em sua composição", num_input);
+        }
+        if ( num_input <= 9) {
+            if ( num_input == 3) {
+                printf("O numero possui o numero 3 em sua composição");
+                return 0;
+                
+            } printf("O numero não possui o numero 3 em sua composição ");
+
+        }
 
     return 0;
 }
