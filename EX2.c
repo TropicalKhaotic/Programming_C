2573fba07c9b2a3d9fc9c3f1721c21940d085619
Rafael <rafavs09@gmail.com>
2024-10-03 10:53:06 -0300

    Old version
    This version of the software, has problems comparing numbers with 0.
    After reading more in how to make this work i will use string comparison.

--- EX2.c
+++ EX2.c
@@ -1,41 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <stdbool.h>
 
 int main(){
 
-    unsigned int num_input = 1000;
+    unsigned int num_input = 1200;
     unsigned int num_comparison = 1000;
     int num_1 = 0;
     int num_2 = 0;
-    int calculation [7] = { 
-    (num_1 = num_input / 100) / 10, 
-    (num_1 = num_input / 100) % 10,
-    (num_1 = num_input % 100) / 10, 
-    (num_1 = num_input % 100) % 10, 
-    num_1 = num_input / 100,
-    num_1 = num_input / 10,
-    num_1 = num_input % 100,
-    };
+    int calculation [7];
+    calculation[0] = (num_1 = num_input / 100) / 10;
+    calculation[1] = (num_1 = num_input / 100) % 10;
+    calculation[2] = (num_1 = num_input % 100) / 10;
+    calculation[3] = (num_1 = num_input % 100) % 10;
+    calculation[4] = num_input / 100;
+    calculation[5] = num_input / 10;
+    calculation[6] = num_input % 100;
+    bool test = false;
+
     int Analizer [2] = {num_comparison / 100, num_comparison % 100};
-    
-    
-    if (num_input >= 1000 && num_input <= 9999 && num_comparison >= 1000 && num_input <= 9999 ) {
+   
+    if (num_input >= 1000 && num_input <= 9999) {
         // Use this if you want to implement user compare input.
         //printf("insert the number you want to compare: ");
         //scanf("%d\n", num_comparison);
-        for (int count = 0; count != 4; count++ ) {
+        for (int count = 0; count <= 4; count++ ) {
             num_2 = calculation [count];
-            printf("%d\n", calculation[count]);
-            if ( num_2 == Analizer[0] / 10) {
-                
-            } if (num_2 == Analizer[0] % 10) {
-                
-            } if (num_2 == Analizer[1] / 10) {
-                
-            } if (num_2 == Analizer[1] % 10) {
-                
-
-            } else { printf("O numero nao possui permutação"); 
+            if ( num_2 == (Analizer[0] / 10) || num_2 == Analizer[0] % 10 || num_2 == Analizer[1] / 10 || num_2 == Analizer[1] % 10) {
+                test = true;
+            } 
+            if ( !test ) { 
+                printf("test");
                 return 0;
             }
         }  
@@ -45,7 +40,7 @@ int main(){
         //printf("insert the number you want to compare: ");
         //scanf("%d\n", num_comparison);
         if ( num_comparison >= 100 && num_comparison <= 999) {  
-            for (int count = 2; count != 4; count++ ) {
+            for (int count = 2; count <= 4; count++ ) {
             num_2 = calculation [count];
             if ( num_2 == Analizer[0] || num_2 == Analizer[1] / 10 ||  num_2 == Analizer[1] % 10) {
                 } else { printf("O numero nao possui permutação"); 
@@ -57,7 +52,7 @@ int main(){
         if ( num_comparison < 100 ); {
             for (int count = 2; count != 4; count++ ) {
                 num_2 = calculation [count];
-                if ( num_2 == Analizer[0] || num_2 == Analizer[1] / 10 ||  num_2 == Analizer[1] % 10) {
+                if ( num_2 == Analizer[0] * 10 ||  num_2 == Analizer[1] * 10) {
 
                 } else { printf("O numero nao possui permutação"); 
                     return 0;
@@ -72,7 +67,7 @@ int main(){
         if (num_comparison >= 0 && num_comparison <= 99) {
                 for (int count = 4; count != 6; count++ ) {
                 num_2 = calculation [count];
-                if ( num_2 == Analizer[0] || num_2 == Analizer[1] / 10 ||  num_2 == Analizer[1] % 10) {
+                if ( num_2 == Analizer[0] || Analizer) {
 
                 } else { printf("O numero nao possui permutação"); 
                     return 0;
@@ -80,6 +75,7 @@ int main(){
             } 
         }
     }
+
 
     printf("O numero: %d , é uma permutação de: %d ", num_comparison, num_input);   
     return 0;
