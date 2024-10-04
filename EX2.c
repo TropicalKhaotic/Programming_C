b2b733a82e9ce17676f68a0bf2ea8511fed4c338
Rafael <rafavs09@gmail.com>
2024-10-04 11:21:05 -0300

    Finished permutation code This code uses a switch and a array to separate numbers and analize

--- EX2.c
+++ EX2.c
@@ -3,80 +3,135 @@
 #include <stdbool.h>
 
 int main(){
+    // Switch selector.
+    int test_input = 0;
+    int test_comparison = 0;
 
-    unsigned int num_input = 1200;
-    unsigned int num_comparison = 1000;
-    int num_1 = 0;
-    int num_2 = 0;
-    int calculation [7];
-    calculation[0] = (num_1 = num_input / 100) / 10;
-    calculation[1] = (num_1 = num_input / 100) % 10;
-    calculation[2] = (num_1 = num_input % 100) / 10;
-    calculation[3] = (num_1 = num_input % 100) % 10;
-    calculation[4] = num_input / 100;
-    calculation[5] = num_input / 10;
-    calculation[6] = num_input % 100;
-    bool test = false;
+    unsigned int num_input = 7845;
+    unsigned int num_comparison = 10000;
+    int num_comp_input [4];
+    int num_comp_compa [4];
+    int calculation [4]; 
 
-    int Analizer [2] = {num_comparison / 100, num_comparison % 100};
-   
-    if (num_input >= 1000 && num_input <= 9999) {
-        // Use this if you want to implement user compare input.
-        //printf("insert the number you want to compare: ");
-        //scanf("%d\n", num_comparison);
-        for (int count = 0; count <= 4; count++ ) {
-            num_2 = calculation [count];
-            if ( num_2 == (Analizer[0] / 10) || num_2 == Analizer[0] % 10 || num_2 == Analizer[1] / 10 || num_2 == Analizer[1] % 10) {
-                test = true;
-            } 
-            if ( !test ) { 
-                printf("test");
-                return 0;
-            }
-        }  
+    // Use this to know if the number is a permutação of another.
+    bool test = true;
+
+    // Use this to know what type of number the user choose.
+    if ( num_input >= 1000 && num_input <= 9999 ) {
+        test_input = 1;
     }
     if ( num_input >= 100 && num_input <= 999 ) {
-        // Use this if you want to implement user compare input.
-        //printf("insert the number you want to compare: ");
-        //scanf("%d\n", num_comparison);
-        if ( num_comparison >= 100 && num_comparison <= 999) {  
-            for (int count = 2; count <= 4; count++ ) {
-            num_2 = calculation [count];
-            if ( num_2 == Analizer[0] || num_2 == Analizer[1] / 10 ||  num_2 == Analizer[1] % 10) {
-                } else { printf("O numero nao possui permutação"); 
-                    return 0;
-                }
-            } 
-        } 
+        test_input = 2;
+    }
+    if ( num_input >= 1 && num_input <= 99 ) {
+        test_input = 3;
+    } 
+    // Use this to know what type of number the user choose.
+    if ( num_comparison >= 1000 && num_comparison <= 9999 ) {
+        test_comparison = 1;
+    }
+    if ( num_comparison >= 100 && num_comparison <= 999 ) {
+        test_comparison = 2;
+    }
+    if ( num_comparison >= 1 && num_comparison <= 99 ) {
+        test_comparison = 3;
+    }
+    if ( num_comparison > 9999 || num_input > 9999) {
+        printf("Não e possivel realizar a conta de permutação com este numero.");
+        return 0;
+    }
+    // Use this to separate the num input into an array.
+    switch (test_input) {
+    case 1:
+        calculation[0] = ( num_input / 100) / 10;
+        calculation[1] = ( num_input / 100) % 10;
+        calculation[2] = ( num_input % 100) / 10;
+        calculation[3] = ( num_input % 100) % 10;
+        for ( int count = 0; count <= 3; count++){
+            num_comp_input [count] = calculation [count];
+        }
+        break;
 
-        if ( num_comparison < 100 ); {
-            for (int count = 2; count != 4; count++ ) {
-                num_2 = calculation [count];
-                if ( num_2 == Analizer[0] * 10 ||  num_2 == Analizer[1] * 10) {
-
-                } else { printf("O numero nao possui permutação"); 
-                    return 0;
-                }
+    case 2:
+        calculation[1] = num_input / 100;
+        calculation[2] = ( num_input % 100) / 10;
+        calculation[3] = ( num_input % 100) % 10;
+        for ( int count = 0; count <= 3; count++){
+            num_comp_input [count] = calculation [count];
+            if ( count < 1) {
+                num_comp_input [count] = 0;
             }
         }
-    }    
-    if ( num_input >= 0 && num_input <= 99 ) {
-        // Use this if you want to implement user compare input.
-        //printf("insert the number you want to compare: ");
-        //scanf("%d\n", num_comparison);
-        if (num_comparison >= 0 && num_comparison <= 99) {
-                for (int count = 4; count != 6; count++ ) {
-                num_2 = calculation [count];
-                if ( num_2 == Analizer[0] || Analizer) {
 
-                } else { printf("O numero nao possui permutação"); 
-                    return 0;
-                }
-            } 
+        break;
+
+    case 3: 
+        calculation[2] = num_input / 10;
+        calculation[3] = num_input % 10;
+        for ( int count = 0; count <= 3; count++){
+            num_comp_input [count] = calculation [count];
+            if ( count <= 1) {
+                num_comp_input [count] = 0;
+            }
+        }
+
+        break;
+
+    }
+    // Use this to separate the num comparison into an array.
+    switch (test_comparison) {
+    case 1:
+        calculation[0] = ( num_comparison / 100) / 10;
+        calculation[1] = ( num_comparison / 100) % 10;
+        calculation[2] = ( num_comparison % 100) / 10;
+        calculation[3] = ( num_comparison % 100) % 10;
+        for ( int count = 0; count <= 3; count++) {
+            num_comp_compa [count] = calculation [count];
+        }
+
+        break;
+    
+    case 2:
+        calculation[1] = num_comparison / 100;
+        calculation[2] = ( num_comparison % 100) / 10;
+        calculation[3] = ( num_comparison % 100) % 10;
+        for ( int count = 0; count <= 3; count++) {
+            num_comp_compa [count] = calculation [count];
+             if ( count < 1) {
+                num_comp_compa [count] = 0;
+            }
+        }
+
+        break;
+
+    case 3:
+        calculation[2] = num_comparison / 10;
+        calculation[3] = num_comparison % 10;
+        for ( int count = 0; count <= 3; count++) {
+            num_comp_compa [count] = calculation [count];
+            if ( count <= 1) {
+                num_comp_compa [count] = 0;
+            }
+            
+        }
+
+        break;
+    }
+    
+    for (int count = 0; count <= 3; count++) {
+        num_comp_input [count];
+        if (num_comp_input [count] == num_comp_compa [0] || num_comp_input [count] == num_comp_compa [1] || num_comp_input [count] == num_comp_compa [2] || num_comp_input [count] == num_comp_compa [3] ) {
+        }
+        if (num_comp_input [count] != num_comp_compa [0] && num_comp_input [count] != num_comp_compa [1] && num_comp_input [count] != num_comp_compa [2] && num_comp_input [count] != num_comp_compa [3] ) {
+            test = false;
         }
     }
 
+    if (test == false) {
+        printf("O numero não possui permutação");
+        return 0;
 
-    printf("O numero: %d , é uma permutação de: %d ", num_comparison, num_input);   
+    } printf("O numero %d e %d possuem permutação", num_input, num_comparison);
+
     return 0;
 }
