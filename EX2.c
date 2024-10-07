59ce556fc47e58c9507e950ee635b75854dd8730
Rafael <rafavs09@gmail.com>
2024-10-07 10:45:00 -0300

    Create a guess game, that show a user classification

--- EX2.c
+++ EX2.c
@@ -1,26 +1,46 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 
-    // Fazer um programa para encontrar todos os pares de números amigáveis entre 1 e 100000.
-    // Um par de números é amigável quando cada um deles é igual à soma dos divisores do outro. 
 
-    int sum_of_divisors(int num) {
-        int sum = 0;
-        for ( int divisor = 1; divisor <= num / 2; divisor++) {
-            if ( num % divisor == 0 ) {
-                sum += divisor;
+
+    int main () {
+        int num = 0;
+        int test = 0;
+        int count = 0;
+        srand(time(0));
+        num = rand() % 500 + 1;
+
+        while (test != num) {
+           printf("Adivinhe o numero do jogo: ");
+           scanf("%d", &test);
+           if (test < num) {
+            printf("O numero digitado é menor que o numero do jogo\n\n");
+            count++;
+           }
+           if (test > num) {
+            printf("O numero digitado é maior que o numero do jogo\n\n");
+            count++;
+            }
+            if (test == num) {
+                printf("Voce acertou!!!\n Sua classificação é: ");
+                switch (count) {
+                    case 1 ... 3 :
+                        printf("Muito sortudo");
+                        break;
+                    case  4 ... 6:
+                        printf("Sortudo");
+                        break;
+                    case 7 ... 10:
+                        printf("Normal");
+                        break; 
+                }
+            }
+            
+            if (count > 10) {
+                printf("Tente novamente");
+                return 0;
             }
         }
-
-        return sum;
-    }
-
-    int main(){
-        for ( int test = 1; test <= 100000; test++) {
-            int calculo = sum_of_divisors(test);
-            if (calculo > test && sum_of_divisors(calculo) == test) {
-                printf("Os numeros são amigaveis: %d e %d\n", test, calculo);
-            }
-        }
-    }
+    }   
