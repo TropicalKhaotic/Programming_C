06fc507aa03b5cd886ca1d095886701dbf536e76
Rafael <rafavs09@gmail.com>
2024-10-07 08:41:02 -0300

    Create a friendly number calculator

--- EX2.c
+++ EX2.c
@@ -3,130 +3,34 @@
 #include <stdbool.h>
 
 int main(){
-    // Switch selector.
-    int test_input = 0;
-    int test_comparison = 0;
+    // Fazer um programa para encontrar todos os pares de números amigáveis entre 1 e 100000.
+    //Um par de números é amigável quando cada um deles é igual à soma dos divisores do outro. 
 
-    unsigned int num_input = 7845;
-    unsigned int num_comparison = 10000;
-    int num_comp_input [4];
-    int num_comp_compa [4];
-    int calculation [4]; 
+    int num = 0;
+    int division = 0;
+    int num_dividend = 0;
 
-    // Use this to know if the number is a permutação of another.
-    bool test = true;
+    for (num = 1; num <= 100000; num++) {
+        for (int num_division = 1; num_division < num; num_division++) {
+            if (num % num_division == 0) {
+                division += num_division;
+            } 
+        }
 
-    // Use this to know what type of number the user choose.
-    if ( num_input >= 1000 && num_input <= 9999 ) {
-        test_input = 1;
-    }
-    if ( num_input >= 100 && num_input <= 999 ) {
-        test_input = 2;
-    }
-    if ( num_input >= 1 && num_input <= 99 ) {
-        test_input = 3;
-    } 
-    // Use this to know what type of number the user choose.
-    if ( num_comparison >= 1000 && num_comparison <= 9999 ) {
-        test_comparison = 1;
-    }
-    if ( num_comparison >= 100 && num_comparison <= 999 ) {
-        test_comparison = 2;
-    }
-    if ( num_comparison >= 1 && num_comparison <= 99 ) {
-        test_comparison = 3;
-    }
-    if ( num_comparison > 9999 || num_input > 9999) {
-        printf("Não e possivel realizar a conta de permutação com este numero.");
-        return 0;
-    }
-    // Use this to separate the num input into an array.
-    switch (test_input) {
-    case 1:
-        calculation[0] = ( num_input / 100) / 10;
-        calculation[1] = ( num_input / 100) % 10;
-        calculation[2] = ( num_input % 100) / 10;
-        calculation[3] = ( num_input % 100) % 10;
-        for ( int count = 0; count <= 3; count++){
-            num_comp_input [count] = calculation [count];
+        for (int num_division = 1; num_division <= num; num_division++) {
+            if (division % num_division == 0) {
+                num_dividend += num_division;
+            } 
         }
-        break;
 
-    case 2:
-        calculation[1] = num_input / 100;
-        calculation[2] = ( num_input % 100) / 10;
-        calculation[3] = ( num_input % 100) % 10;
-        for ( int count = 0; count <= 3; count++){
-            num_comp_input [count] = calculation [count];
-            if ( count < 1) {
-                num_comp_input [count] = 0;
-            }
-        }
-        break;
-
-    case 3: 
-        calculation[2] = num_input / 10;
-        calculation[3] = num_input % 10;
-        for ( int count = 0; count <= 3; count++){
-            num_comp_input [count] = calculation [count];
-            if ( count <= 1) {
-                num_comp_input [count] = 0;
-            }
-        }
-        break;
-
-    }
-    // Use this to separate the num comparison into an array.
-    switch (test_comparison) {
-    case 1:
-        calculation[0] = ( num_comparison / 100) / 10;
-        calculation[1] = ( num_comparison / 100) % 10;
-        calculation[2] = ( num_comparison % 100) / 10;
-        calculation[3] = ( num_comparison % 100) % 10;
-        for ( int count = 0; count <= 3; count++) {
-            num_comp_compa [count] = calculation [count];
-        }
-        break;
-    
-    case 2:
-        calculation[1] = num_comparison / 100;
-        calculation[2] = ( num_comparison % 100) / 10;
-        calculation[3] = ( num_comparison % 100) % 10;
-        for ( int count = 0; count <= 3; count++) {
-            num_comp_compa [count] = calculation [count];
-             if ( count < 1) {
-                num_comp_compa [count] = 0;
-            }
-        }
-        break;
-
-    case 3:
-        calculation[2] = num_comparison / 10;
-        calculation[3] = num_comparison % 10;
-        for ( int count = 0; count <= 3; count++) {
-            num_comp_compa [count] = calculation [count];
-            if ( count <= 1) {
-                num_comp_compa [count] = 0;
-            }
-            
-        }
-        break;
-    }
-    
-    for (int count = 0; count <= 3; count++) {
-        num_comp_input [count];
-        if (num_comp_input [count] == num_comp_compa [0] || num_comp_input [count] == num_comp_compa [1] || num_comp_input [count] == num_comp_compa [2] || num_comp_input [count] == num_comp_compa [3] ) {
-        }
-        if (num_comp_input [count] != num_comp_compa [0] && num_comp_input [count] != num_comp_compa [1] && num_comp_input [count] != num_comp_compa [2] && num_comp_input [count] != num_comp_compa [3] ) {
-            test = false;
+        if ( num_dividend == num) {
+            printf("São amigaveis os numeros %d e %d\n", division, num_dividend);
+        } 
+        
+        if ( num_dividend != num) {
+            division = 0;
+            num_dividend = 0;
         }
     }
-
-    if (test == false) {
-        printf("O numero não possui permutação");
-        return 0;
-
-    } printf("O numero %d e %d possuem permutação", num_input, num_comparison);
-
     return 0;
 }
