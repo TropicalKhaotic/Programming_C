42e3f2c3a37331b56b4f37a43d54eaad7b46a053
Rafael <rafavs09@gmail.com>
2024-10-07 11:56:27 -0300

    Creating a prime calculator based on user input

--- EX2.c
+++ EX2.c
@@ -2,42 +2,36 @@
 #include<stdlib.h>
 #include<time.h>
 
-    int main () {
-        int num = 0;
-        int test = 0;
-        int count = 0;
-        srand(time(0));
-        num = rand() % 500 + 1;
 
-        while (test != num) {
-           printf("Adivinhe o numero do jogo: ");
-           scanf("%d", &test);
-           if (test < num) {
-            printf("O numero digitado é menor que o numero do jogo\n\n");
-            count++;
-           }
-           if (test > num) {
-            printf("O numero digitado é maior que o numero do jogo\n\n");
-            count++;
-            }
-            if (test == num) {
-                printf("Voce acertou!!!\n Sua classificação é: ");
-                switch (count) {
-                    case 1 ... 3 :
-                        printf("Muito sortudo");
-                        break;
-                    case  4 ... 6:
-                        printf("Sortudo");
-                        break;
-                    case 7 ... 10:
-                        printf("Normal");
-                        break; 
+    int prime_calculation(int num) {
+        int num_start = 2;
+        int num_test = 0;
+        int contador = 0;
+        int number = 0;
+
+        while (num_start < num) {
+            for(int division = 1; division <= num_start; division++) {
+                num_test = num_start % division;
+                if (num_test == 0) {
+                    contador++;
                 }
             }
-
-            if (count > 10) {
-                printf("Tente novamente");
-                return 0;
+            if (contador == 2) {
+                number = num_start;
             }
+            contador = 0;
+            num_start++;
         }
+        return number; 
+    }
+
+
+    int main () {
+
+        int num_input = 0;
+
+        printf("Insira um numero a ser calculado: ");
+        scanf("%d", &num_input);
+        printf("O numero primo mais alto é: %d", prime_calculation(num_input));
+        
     }   
