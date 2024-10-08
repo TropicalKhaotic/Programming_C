01fdaa7fc7167f2a2a9bd17453e93a949ea58aa3
Rafael <rafavs09@gmail.com>
2024-10-08 09:18:55 -0300

    Use this to print the MMC of two integers

--- EX2.c
+++ EX2.c
@@ -2,34 +2,39 @@
 #include<stdlib.h>
 
 
-    int prime_calculation(int num) {
-        int num_start = 2;
-        int num_test = 0;
-        int contador = 0;
-        int number = 0;
+    int MMC_calculator(int num, int num_2) {
+        int num_rest = 0;
+        int num_rest2 = 0;
+        int prime_numbers[8] = { 2, 3, 5, 7, 11, 13, 17, 19};
+        int count = 0;
+        while ( num != 1 || num_2 != 1 ) {
+            num_rest = num % prime_numbers[count];
+            num_rest2 = num_2 % prime_numbers[count];
 
-        while (num_start < num) {
-            for(int division = 1; division <= num_start; division++) {
-                num_test = num_start % division;
-                if (num_test == 0) {
-                    contador++;
-                }
+            if (num_rest == 0 && num_rest2 == 0) {
+                num = num / prime_numbers[count];
+                num_2 = num_2 / prime_numbers[count];
+                printf("%d, ", prime_numbers[count]);
+                count = 0;
             }
-            if (contador == 2) {
-                number = num_start;
+            if (num_rest != 0 || num_rest2 != 0) {
+                if (count == 7) {
+                    break;
+                } 
+                count++;
             }
-            contador = 0;
-            num_start++;
-        }
-        return number; 
-    }
-
-
+        } 
+    return 1;
+    }  
     int main () {
 
-        int num_input = 0;
+        int num_input1 = 0;
         printf("Insira um numero a ser calculado: ");
-        scanf("%d", &num_input);
-        printf("O numero primo mais alto é: %d", prime_calculation(num_input));
-        
+        scanf("%d", &num_input1);
+        int num_input2 = 0;
+        printf("Insira um numero a ser calculado: ");
+        scanf("%d", &num_input2);
+        printf("Os multiplos simultaneos de %d e %d são: \n", num_input1,num_input2);
+        printf("%d",  MMC_calculator(num_input1,num_input2));
+
     }   
