43727e7d527140a48362153bd4e76cf8fd78a56e
Rafael <rafavs09@gmail.com>
2024-10-08 11:40:50 -0300

    Create a number analizer it works with smal number and non repeated numbers need to fix problems with repeated number and with printing 0

--- EX2.c
+++ EX2.c
@@ -1,39 +1,53 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-    int MMC_calculator(int num, int num_2) {
-        int num_rest = 0;
-        int num_rest2 = 0;
-        int prime_numbers[8] = { 2, 3, 5, 7, 11, 13, 17, 19};
-        int count = 0;
-        while ( num != 1 || num_2 != 1 ) {
-            num_rest = num % prime_numbers[count];
-            num_rest2 = num_2 % prime_numbers[count];
-
-            if (num_rest == 0 && num_rest2 == 0) {
-                num = num / prime_numbers[count];
-                num_2 = num_2 / prime_numbers[count];
-                printf("%d, ", prime_numbers[count]);
-                count = 0;
-            }
-            if (num_rest != 0 || num_rest2 != 0) {
-                if (count == 7) {
-                    break;
-                } 
-                count++;
-            }
-        } 
-    return 1;
-    }  
     int main () {
 
-        int num_input1 = 0;
-        printf("Insira um numero a ser calculado: ");
-        scanf("%d", &num_input1);
-        int num_input2 = 0;
-        printf("Insira um numero a ser calculado: ");
-        scanf("%d", &num_input2);
-        printf("Os multiplos simultaneos de %d e %d são: \n", num_input1,num_input2);
-        printf("%d",  MMC_calculator(num_input1,num_input2));
+        /*Escreva um programa no qual o usuário digita
+        uma quantidade arbitrária de números inteiros positivos. Quando o usuário digitar um
+        número menor ou igual a 0, o programa deve indicar o tamanho da maior sequência
+        crescente observada.*/
 
+        int array[15];
+        int user_input = 1;
+        int count = 0;
+        int print = 0;
+        int cres_num[15];
+
+        for (int pos = 0; user_input > 0; pos++) { 
+            if (print == 14 || user_input <= 0) {
+                if ( user_input <= 0) {
+                    break;
+                    
+                }
+                if (print == 15) {
+                    printf("Não suportado");
+                    return 0;
+                }
+            }
+            printf("Inser a number: \n");
+            scanf("%d", &user_input);
+            print++;
+            array[pos] = user_input;
+            count++;
+
+        }
+        printf("\n");
+        for (int compare = 0; compare < print; compare++) {
+            count = 0;
+            for (int i = 0; i < print; i++ ) {
+                if (array[compare] >= array[i]) {
+                    count++;
+                }
+            }
+            cres_num[count] = array[compare];
+            printf("%d, ", cres_num[count]);
+        }
+        printf("A maior combinação crescente é: ");
+        int i = 0;
+        while ( i < print) {
+            printf("%d, ", cres_num[i]);
+            i++;
+        }
+    return 0;
     }   
